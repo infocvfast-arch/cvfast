44ac90dbc7184c28a0b1a43dd09432e3684e41cd
infocvfast-arch <infocvfast@gmail.com>
2026-08-10 19:05:34 +0200

    Update paypal-config.js

--- api/paypal-config.js
+++ api/paypal-config.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+  const clientId = process.env.PAYPAL_CLIENT_ID;
 
+  if (!clientId) {
+    return res.status(500).json({
+      error: "PayPal is not configured"
+    });
+  }
+
+  res.status(200).json({
+    clientId
+  });
+}
